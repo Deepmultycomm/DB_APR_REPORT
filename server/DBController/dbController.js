ee035009250d3d7e37e049e7c4a80e7735cfd051
Deepmultycomm <deep@multycomm.com>
2025-09-11 13:23:34 +0530

    Delete server directory

--- server/DBController/dbController.js
+++
@@ -1,7 +0,0 @@
-import {fetchAPRData,fetchAgentStatusData} from "../Collecetions/fetchAPIToMergeData.js";
-
-const agent_events = await fetchAPRData("1755678600", "1755693000");
-const users_calls = await fetchAgentStatusData("1755678600", "1755693000");
-
-console.log("agent_events:", agent_events);
-console.log("users_calls:", users_calls);
